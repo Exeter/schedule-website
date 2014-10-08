861e1871528a657047ecec9ab452205f788cd45e
dabbler0 <dab1998@gmail.com>
2014-10-08 19:28:46 -0400

    Example daily event

--- build/index.js
+++ build/index.js
@@ -1,1 +1,1 @@
-var calendar=$("#calendar").calendar({tmpl_path:"bower_components/bootstrap-calendar/tmpls/",view:"week",events_source:function(){return[]}});
+var calendar=$("#calendar").calendar({tmpl_path:"bower_components/bootstrap-calendar/tmpls/",view:"week",events_source:"/events"});
--- src/server.js
+++ src/server.js
@@ -4,7 +4,43 @@ var express = require('express'),
 
 var app = express();
 
+REGULAR_EVENTS = [
+  {
+    id: 0,
+    name: 'Daily Event',
+    start: 1412809833621,
+    duration: 3600000,
+    frequency: 86400000
+  }
+];
+
 app.use('/bower_components', express.static('bower_components'));
+
+app.get('/events', function(req, res, next) {
+  var from = Number(req.query.from),
+      to = Number(req.query.to), ev, st,
+      events = [];
+  for (var i = 0; i < REGULAR_EVENTS.length; i++) {
+    ev = REGULAR_EVENTS[i];
+    st = Math.floor((from - ev.start) / ev.frequency); startTime = 0;
+    while (startTime < to) {
+      startTime = ev.start + ev.frequency * st;
+      events.push({
+        id: ev.id.toString() + '@' + startTime,
+        title: ev.name,
+        class: 'event-important',
+        start: startTime,
+        end: startTime + ev.duration
+      });
+      st += 1;
+    }
+  }
+  res.send(JSON.stringify({
+    success: 1,
+    result: events
+  }));
+});
+
 app.use('/', express.static('.'));
 
 app.listen(process.env.PORT, function() {
