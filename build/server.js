409a0b27eb6aa3db439507051ab9f1efa143d4a7
Alec Sun <glakeon@gmail.com>
2014-10-09 22:05:41 -0400

    added event "morning class" as test

--- build/server.js
+++ build/server.js
@@ -1,1 +1,65 @@
-var express=require("express"),fs=require("fs"),url=require("url"),app=express();REGULAR_EVENTS=[{id:0,name:"Daily Event",color:"#A6B9C4",start:1412809833621,duration:36e5,frequency:864e5},{id:1,name:"Odd-Hours Event",color:"#EDF393",start:1412802633621,duration:72e5,frequency:1728e5}],app.use("/bower_components",express.static("bower_components")),app.get("/events",function(a,b){for(var c,d,e=Number(a.query.from),f=Number(a.query.to),g=[],h=0;h<REGULAR_EVENTS.length;h++)for(c=REGULAR_EVENTS[h],d=Math.floor((e-c.start)/c.frequency),startTime=0;f>startTime;)startTime=c.start+c.frequency*d,g.push({id:c.id.toString()+"@"+startTime,title:c.name,color:c.color,start:startTime,end:startTime+c.duration}),d+=1;b.send(JSON.stringify({success:1,result:g}))}),app.use("/",express.static(".")),app.listen(process.env.PORT,function(){console.log("Listening on port",process.env.PORT)});
+var express = require('express'),
+    fs = require('fs'),
+    url = require('url');
+
+var app = express();
+
+REGULAR_EVENTS = [
+  {
+    id: 0,
+    name: 'Daily Event',
+    color: '#A6B9C4',
+    start: 1412809833621,
+    duration: 3600000,
+    frequency: 86400000
+  },
+  {
+    id: 1,
+    name: 'Odd-Hours Event',
+    color: '#EDF393',
+    start: 1412802633621,
+    duration: 7200000,
+    frequency: 172800000
+  },
+  {
+    id: 2,
+    name: 'Morning Class',
+    color: '#EDF393',
+    start: 1412799033621,
+    duration: 3600000,
+    frequency: 172800000
+  }
+];
+
+app.use('/bower_components', express.static('bower_components'));
+
+app.get('/events', function(req, res, next) {
+  var from = Number(req.query.from),
+      to = Number(req.query.to), ev, st,
+      events = [];
+  for (var i = 0; i < REGULAR_EVENTS.length; i++) {
+    ev = REGULAR_EVENTS[i];
+    st = Math.floor((from - ev.start) / ev.frequency); startTime = 0;
+    while (startTime < to) {
+      startTime = ev.start + ev.frequency * st;
+      events.push({
+        id: ev.id.toString() + '@' + startTime,
+        title: ev.name,
+        color: ev.color,
+        start: startTime,
+        end: startTime + ev.duration
+      });
+      st += 1;
+    }
+  }
+  res.send(JSON.stringify({
+    success: 1,
+    result: events
+  }));
+});
+
+app.use('/', express.static('.'));
+
+app.listen(process.env.PORT, function() {
+  console.log('Listening on port', process.env.PORT);
+});
