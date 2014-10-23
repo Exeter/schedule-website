58baacfe533742b7907c7edb1b1f61fec6f60e3f
alec <glakeon@gmail.com>
2014-10-22 23:19:55 -0400

    fix grunt complains

--- src/server.js
+++ src/server.js
@@ -8,32 +8,40 @@ var year_obj = JSON.parse(fs.readFileSync('year.json')),
     week1_obj = JSON.parse(fs.readFileSync('week1.json')),
     week2_obj = JSON.parse(fs.readFileSync('week2.json')),
     weeks = {
-      week1: week1_obj,
-      week2: week2_obj
+        week1: week1_obj,
+        week2: week2_obj
     },
     defaults_obj = JSON.parse(fs.readFileSync('defaults.json'));
 
 function padTwo(n) {
-  var str = n.toString();
-  if (str.length === 1) return '0' + str;
-  else return str;
+    var str = n.toString();
+    if (str.length === 1) return '0' + str;
+    else return str;
 }
 
 // Maps formats to colors
 var format_to_color = {
-  "a": "#F7FE2E", "t": "#F7FE2E",
-  "b": "#58FA58", "u": "#58FA58",
-  "c": "#2ECCFA", "v": "#2ECCFA",
-  "d": "#CC2EFA", "w": "#CC2EFA",
-  "e": "#FE2E2E", "x": "#FE2E2E",
-  "f": "#088A08", "y": "#088A08",
-  "g": "#FF8000", "z": "#FF8000",
-  "h": "#045FB4", "q": "#045FB4",
-  "*": "#BDBDBD"
+    "a": "#F7FE2E",
+    "t": "#F7FE2E",
+    "b": "#58FA58",
+    "u": "#58FA58",
+    "c": "#2ECCFA",
+    "v": "#2ECCFA",
+    "d": "#CC2EFA",
+    "w": "#CC2EFA",
+    "e": "#FE2E2E",
+    "x": "#FE2E2E",
+    "f": "#088A08",
+    "y": "#088A08",
+    "g": "#FF8000",
+    "z": "#FF8000",
+    "h": "#045FB4",
+    "q": "#045FB4",
+    "*": "#BDBDBD"
 };
 
 for (var key in format_to_color) {
-  format_to_color[key.toUpperCase()] = format_to_color[key];
+    format_to_color[key.toUpperCase()] = format_to_color[key];
 }
 
 // Convenience constants
@@ -42,39 +50,39 @@ var MS_PER_MIN = 60000;
 // Convenience function for converting
 // period-delimited time representation to epoch time.
 function time_to_ms(year, month, day, hour, minute) {
-  return (new Date(year, month, day, hour, minute, 0, 0)).getTime();
+    return (new Date(year, month, day, hour, minute, 0, 0)).getTime();
 }
 
 function ms_to_date(ms) {
-  var d = new Date(0);
-  d.setUTCSeconds(ms / 1000);
-  return d;
+    var d = new Date(0);
+    d.setUTCSeconds(ms / 1000);
+    return d;
 }
 
 function getMonDate(ms) {
-  var d = ms_to_date(ms),
-      day = d.getDay(),
-      diff = d.getDate() - day + (day === 0 ? -6 : 1);
-  d.setDate(diff);
-  if (d.getDay() !== 1) {
-    throw new Error('WRONG');
-  }
-  return d;
+    var d = ms_to_date(ms),
+        day = d.getDay(),
+        diff = d.getDate() - day + (day === 0 ? -6 : 1);
+    d.setDate(diff);
+    if (d.getDay() !== 1) {
+        throw new Error('WRONG');
+    }
+    return d;
 }
 
 function getMonDay(ms) {
-  var d = getMonDate(ms);
-  return padTwo(d.getMonth() + 1) + '.' + padTwo(d.getDate()) + '.' + d.getFullYear();
+    var d = getMonDate(ms);
+    return padTwo(d.getMonth() + 1) + '.' + padTwo(d.getDate()) + '.' + d.getFullYear();
 }
 
 function getMonTime(ms) {
-  var d = getMonDate(ms);
-  return d.getTime();
+    var d = getMonDate(ms);
+    return d.getTime();
 }
 
 function getWeekDay(ms) {
-  var d = ms_to_date(ms);
-  return d.getDay();
+    var d = ms_to_date(ms);
+    return d.getDay();
 }
 
 /*
@@ -84,43 +92,41 @@ function getWeekDay(ms) {
 var all_events = {};
 
 function getDuration(event) {
-  if (event.match(/^[abcdefghituvwxyzq]$/) != null) {
-    return defaults_obj.symbols.format.lowercase * MS_PER_MIN;
-  }
-  else if (event.match(/^[ABCDEFGHI]$/) != null) {
-    return defaults_obj.symbols.format.uppercase * MS_PER_MIN;
-  }
-  else {
-    return defaults_obj.symbols[event].minutes * MS_PER_MIN;
-  }
+    if (event.match(/^[abcdefghituvwxyzq]$/) !== null) {
+        return defaults_obj.symbols.format.lowercase * MS_PER_MIN;
+    } else if (event.match(/^[ABCDEFGHI]$/) !== null) {
+        return defaults_obj.symbols.format.uppercase * MS_PER_MIN;
+    } else {
+        return defaults_obj.symbols[event].minutes * MS_PER_MIN;
+    }
 }
 
 function getColor(event) {
-  if (event in format_to_color) {
-    return format_to_color[event];
-  }
-  else {
-    return format_to_color['*'];
-  }
+    if (event in format_to_color) {
+        return format_to_color[event];
+    } else {
+        return format_to_color['*'];
+    }
 }
 
 function generateEvents(start, week) {
-  var i, event, duration,
-      weekEvents = ((week.days[getWeekDay(start)] || {}).events || []),
-      startTime = start + 1000 * 60 * 60 * 8,
-      generatedEvents = [];
-  for (i = 0; i < weekEvents.length; i++) {
-    event = weekEvents[i]; duration = getDuration(event);
-    generatedEvents.push({
-      id: Math.random(),
-      title: event,
-      color: getColor(event),
-      start: startTime,
-      end: startTime + duration
-    });
-    startTime += duration;
-  }
-  return generatedEvents;
+    var i, event, duration,
+        weekEvents = ((week.days[getWeekDay(start)] || {}).events || []),
+        startTime = start + 1000 * 60 * 60 * 8,
+        generatedEvents = [];
+    for (i = 0; i < weekEvents.length; i++) {
+        event = weekEvents[i];
+        duration = getDuration(event);
+        generatedEvents.push({
+            id: Math.random(),
+            title: event,
+            color: getColor(event),
+            start: startTime,
+            end: startTime + duration
+        });
+        startTime += duration;
+    }
+    return generatedEvents;
 }
 
 console.log(year_obj.schedules['10.13.2014']);
@@ -130,19 +136,19 @@ var app = express();
 app.use('/bower_components', express.static('bower_components'));
 
 app.get('/events', function(req, res, next) {
-  var from = Number(req.query.from),
-      to = Number(req.query.to),
-      mon = getMonDay(from),
-      asdf = console.log('looking up', mon, year_obj.schedules[mon]),
-      events = generateEvents(from, weeks[year_obj.schedules[getMonDay(from)].calendar]);
-  res.send(JSON.stringify({
-    success: true,
-    result: events
-  }));
+    var from = Number(req.query.from),
+        to = Number(req.query.to),
+        mon = getMonDay(from),
+        asdf = console.log('looking up', mon, year_obj.schedules[mon]),
+        events = generateEvents(from, weeks[year_obj.schedules[getMonDay(from)].calendar]);
+    res.send(JSON.stringify({
+        success: true,
+        result: events
+    }));
 });
 
 app.use('/', express.static('.'));
 
 app.listen(process.env.PORT, function() {
-  console.log('Listening on port', process.env.PORT);
+    console.log('Listening on port', process.env.PORT);
 });
